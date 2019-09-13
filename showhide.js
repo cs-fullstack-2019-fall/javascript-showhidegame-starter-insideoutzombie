1829adb468860b2613a25c3d9cf0b92a5e9f29ad
insideoutzombie <david@web3devs.com>
2019-09-13 11:14:23 -0500

    progress made image renders with random function logic to render images

--- showhide.js
+++ showhide.js
@@ -3,3 +3,26 @@ let player_clicks = 0;
 let player_score = 0;
 const delay = 2000;
 const targets = 5;
+
+function main() {
+  player_clicks = 0;
+  player_score = 0;
+  // get random spots on the board
+  for (let i = 1; i <= 5; i++) {
+
+    let target_num = getRandomInt(1,50);
+    let target_cell_id = `td${target_num}`;
+    let image_id = `img${target_num}`;
+    // console.log(target_cell_id);
+    $('#' + target_cell_id).append(`<img id=${image_id} src=fish.png />`);
+  }
+}
+
+// Utility function to get a random table cell number
+function getRandomInt(min, max) {
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+// Start the game!!!
+main();
